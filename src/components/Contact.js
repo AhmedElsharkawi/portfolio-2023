c8598067054de601a00e518733cc5025cad986bf
ahmedelsharkawi <ahmed.elshrkawy3681@gmail.com>
2024-09-16 00:30:34 +0200

    correct contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,7 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_gfdd0zd', 'template_g74id2q', form.current, 'UcXiA59iCdeZGpvKw')
+    emailjs.sendForm('service_gfdd0zd', 'template_g74id2q', form.current, 'pR5BEOnn7ucsjrUrr')
       .then((result) => {
         console.log(result.text);
         e.target.reset()
@@ -30,13 +30,13 @@ const Contact = () => {
         <div className='form'>
           <form ref={form} onSubmit={sendEmail}>
             <label>Your Name</label>
-            <input type='text' name="name" />
+            <input type='text' name="name"  required/>
             <label>Email</label>
-            <input type="email" name="email" />
+            <input type="email" name="email" required/>
             <label>Subject</label>
-            <input type='text' />
+            <input type='text' required/>
             <label>Message</label>
-            <textarea rows={6} placeholder='Type your message here' name="message" />
+            <textarea rows={6} placeholder='Type your message here' name="message" required />
             <button className='btn' onSubmit={sendEmail}>Submit</button>
           </form>
 
